97035a36b42d08ec31d00e7235db82a3e6f4ef73
Wzb <1391376741@qq.com>
2022-09-23 11:20:43 +0800

    Initial commit

---
+++ src/main/java/com/hnkjxy/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.hnkjxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorld {
+    public static void main(String[] args) {
+        //启动Spring应用
+        SpringApplication.run(HelloWorld.class,args);
+    }
+}
